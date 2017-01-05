39fb9b018f275f87b33c34ec4fa61833cf6185c8
rachaelcodes <rachaelcodes@users.noreply.github.com>
2017-01-05 21:32:29 +0000

    updated container formatting and added new colour change option

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ function setBackground() {
 	document.body.style.background = bgColor;
 }
 
+function setFocus() {
+	var focusColor = getColor();
+	document.getElementById("example").style.background = focusColor;
+}
+
+function setHeading() {
+	var headingColor = getColor();
+	document.getElementById("heading").style.color = headingColor;
+}
+
 document.body.onkeyup = function(x) {
-if (x.keyCode==69 || x.keyCode==82) {setBackground();}
+if (x.keyCode==66) {setBackground();}
+if (x.keyCode==70) {setFocus();}
+if (x.keyCode==72) {setHeading();}
 }
